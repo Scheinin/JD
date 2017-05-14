767eafe518f3150174a61e80ac6e1c370cfb3582
Scheinin <Scheinin@163.com>
2017-05-14 18:05:47 +0800

    0514

--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@
 
 
 $(function() {
+	// 网站导航二级菜单位置
+	$('#ttbar-navs .dd').css('right',(1190-$(document).width())/2+'px');
+	// 手机京东下拉
 	$('#ttbar-mob').hover(
 		function() {
 			$('#shortcut .mobile_pop'). slideDown(function() {
@@ -13,6 +16,29 @@ $(function() {
 			$('#ttbar-mob .triangle-down').css('border-bottom','8px solid #f6f6f6')
 		}
 		)
+	//搜索框
+	var text = $('.search-m .text').val();
+	$('.search-m .text').bind('focus', function() {
+		if($(this).val()) {
+			$(this).val('');
+		}
+	}).bind('blur', function() {
+		$(this).val(text);
+	})
+	// 我的购物车
+	$('#settleup').hover(
+		function() {
+		$(this).css('border','none');
+		},
+		function(){
+		$(this).css('border','1px solid #e3e4e5');
+		}
+	)
+
+
+
+
+	
 
 
 
